68d4f2e1d18e996ee8bd0dabd3bb5ea7dba5b506
Hanan khan <hkshawls956@gmail.com>
2025-06-27 13:53:51 +0530

    scroll issue resolved

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import { HeroSecrion } from './components/HeroSection'
 import { Routes,Route } from 'react-router-dom'
 import { Register } from './Pages/Register'
 import { LogIn } from './Pages/LogIn'
+import { ScrollToTop } from './Scroll/ScrollTop'
 
 
 function App() {
  
 
   return (
-<>
+<>           <ScrollToTop />
              <Routes>
               <Route path='/' element={< HeroSecrion />} />
               <Route path='/register' element={<Register />} />
